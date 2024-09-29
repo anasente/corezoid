6128696cf2adcc84fd2cbe9c2b3534e19cce3b52
anasente <alexdienstag4589@gmail.com>
2024-09-29 04:03:58 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -10,7 +10,7 @@ module.exports = async (data) => {
 
         request = {
             method: 'get',
-            url: 'https://blockchain.info/ticker'
+            url: 'https://blockchain.info/ticke'
         };
 
         response = await axios(request);
