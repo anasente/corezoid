9528cbd3b4c9832146a523feac3288b4aeae53b1
anasente <alexdienstag4589@gmail.com>
2024-09-29 03:50:41 +0300

    Create usercode.js

---
+++ usercode.js
@@ -0,0 +1,28 @@
+// build command npm i axios
+
+const axios = require('axios');
+
+module.exports = async (data) => {
+
+    var request, response;
+
+    try {
+
+        request = {
+            method: 'get',
+            url: 'https://blockchain.info/ticker'
+        };
+
+        response = await axios(request);
+
+    } catch (error) {
+        response = error.response || {
+            error: 'Request failed'
+        };
+    }
+
+    return {
+        "request": request,
+        "response": response.data
+    };
+};
